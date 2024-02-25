391ef65b5ab6c3df03bc5cea9b082fce98bf3397
Hendri Juhanda <hendri.juhanda@gmail.com>
2024-02-25 10:09:45 +0700

    feat: WIP intergrate api

--- next-nodejs/src/components/pages/home/LatestProducts.tsx
+++ next-nodejs/src/components/pages/home/LatestProducts.tsx
@@ -7,7 +7,7 @@ import { InnerWrapper } from "@/components/ui/layouts";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { Product } from ".";
 
@@ -15,8 +15,26 @@ export const LatestProducts = () => {
   let sliderRef = useRef(null);
 
   const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [isFetched, setIsFetched] = useState<boolean>();
+  const [items, setItems] = useState<any[]>([]);
 
-  const items = Array.from(Array(10).keys());
+  useEffect(() => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/client/products?${new URLSearchParams(
+        {
+          sort: "created_at,desc",
+          page: "1",
+          per_page: "8",
+        }
+      )}`
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        setIsFetched(true);
+        setItems(body.data.items);
+      })
+      .catch((e) => {});
+  }, []);
 
   const next = () => {
     // @ts-ignore
@@ -39,48 +57,52 @@ export const LatestProducts = () => {
 
   return (
     <section className="my-8">
-      <InnerWrapper>
-        <h3 className={`${playfairDisplay.className} text-2xl font-bold mb-4`}>
-          Terbaru
-        </h3>
-
-        <div className="relative">
-          <Slider
-            ref={(slider: any) => {
-              sliderRef = slider;
-            }}
-            {...settings}
+      {isFetched && (
+        <InnerWrapper>
+          <h3
+            className={`${playfairDisplay.className} text-2xl font-bold mb-4`}
           >
-            {items.map(() => (
-              <div className="px-2 pb-8">
-                <Product
-                  imageUrl="/product-1.png"
-                  name="Exodia"
-                  price={9000000}
-                />
+            Terbaru
+          </h3>
+
+          <div className="relative">
+            <Slider
+              ref={(slider: any) => {
+                sliderRef = slider;
+              }}
+              {...settings}
+            >
+              {items.map((item) => (
+                <div className="px-2 pb-8">
+                  <Product
+                    imageUrl={item.image_url}
+                    name={item.name}
+                    price={item.price}
+                  />
+                </div>
+              ))}
+            </Slider>
+
+            {currentSlide !== 0 && (
+              <div
+                className="absolute top-[50%] -mt-10 -ml-16 w-20 h-20 flex items-center justify-center text-5xl cursor-pointer"
+                onClick={previous}
+              >
+                <MdChevronLeft />
               </div>
-            ))}
-          </Slider>
+            )}
 
-          {currentSlide !== 0 && (
-            <div
-              className="absolute top-[50%] -mt-10 -ml-16 w-20 h-20 flex items-center justify-center text-5xl cursor-pointer"
-              onClick={previous}
-            >
-              <MdChevronLeft />
-            </div>
-          )}
-
-          {currentSlide !== items.length - 5 && (
-            <div
-              className="absolute top-[50%] right-0 -mt-10 -mr-16 w-20 h-20 flex items-center justify-center text-5xl cursor-pointer"
-              onClick={next}
-            >
-              <MdChevronRight />
-            </div>
-          )}
-        </div>
-      </InnerWrapper>
+            {currentSlide !== items.length - 5 && (
+              <div
+                className="absolute top-[50%] right-0 -mt-10 -mr-16 w-20 h-20 flex items-center justify-center text-5xl cursor-pointer"
+                onClick={next}
+              >
+                <MdChevronRight />
+              </div>
+            )}
+          </div>
+        </InnerWrapper>
+      )}
     </section>
   );
 };
